04cd6c013b2771a91ad936ab01db09c4b0d66b1a
Bub-Bob <bubbob18@gmail.com>
2024-04-14 23:39:13 -0700

    Added dot product
    Added the tutorials main scene.
    Added a mechanic to turn the player towards the mouse using dot products.

--- Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,6 +16,10 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
+    Vector3 center_screen;
+    Vector3 to_mouse;
+    float desired_Angle;
+
     void Start ()
     {
         m_Animator = GetComponent<Animator> ();
@@ -26,6 +31,44 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate ()
     {
+        /* The character is always in the center of the screen.
+         * By comparing the position of the mouse to the center of the screen a vector describing the 
+         * position of the mouse to the character can be created.
+         * 
+         * To find the angle created by this vector compared to the angle of the world
+         * we take the dot product of the vector and a reference vector that is 0 degrees from the world angle
+         * , divide it by the product of the two vectors magnitudes and running this through arccos.
+         * 
+         * We will use the vector created by subtracting the center of the screen vector from the mouse position vector
+         * and call this vector a.
+         * a = [ x
+         *       y 
+         *       (z = 0) ]
+         * The refrence vector will be i.
+         * i = [ 0
+         *       1
+         *       0 ]
+         * so the dot product of these vectors will always come to be y.
+         * i's magnitude is obviously 1.
+         * if we normalize a then its magnitude will also be 1.
+         * this means arccos(a*i/||a||*||i||) can be simplified to arccos(y)
+         */
+        center_screen.Set((Screen.width/2), (Screen.height/2), 0);
+        to_mouse = Input.mousePosition - center_screen;
+        to_mouse.Normalize();
+        desired_Angle = (float) Math.Acos(to_mouse.y);
+
+        // Converts radian angle to degrees and tracks which half it is on.
+        desired_Angle = (180f / (float) Math.PI) * desired_Angle;
+        if (to_mouse.x < 0) 
+        { 
+            desired_Angle = desired_Angle * -1; 
+        }
+
+        //
+        //Debug.Log(desired_Angle);
+        m_Rotation.eulerAngles = new Vector3(0f, desired_Angle, 0f);
+
         var pos = MoveAction.ReadValue<Vector2>();
         
         float horizontal = pos.x;
@@ -51,8 +94,6 @@ public class PlayerMovement : MonoBehaviour
             m_AudioSource.Stop ();
         }
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
     }
 
     void OnAnimatorMove ()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JohnLemon
  Assignment 2 for CS410
+
+Dot product: Added a mechanic so the MC looks towards the mouse during play. This Uses a dot product to convert a vector derived from the mouse position and the center of the screen 
+to an angle based off of an identity vector based on the worlds y rotation. More is written in the PlayerMovement script.
